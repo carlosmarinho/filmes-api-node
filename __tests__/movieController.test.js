8dfcca9cb38067bc15cfd9ab9e153081f39ec938
Carlos Marinho <carlos.marinho@mytime.com>
2022-11-11 21:41:46 -0300

    creste should call Service.findAll()

--- __tests__/movieController.test.js
+++ __tests__/movieController.test.js
@@ -1,6 +1,25 @@
+const httpMocks = require("node-mocks-http");
+
 const MovieController = require("../app/controllers/movieController")
+const MovieService = require("../app/services/movieService")
+
+jest.mock("../app/services/movieService")
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  // next = null;
+  //We are  going to use the next on the error unit test so we will use jest.fn()
+  next = jest.fn();
+});
+
 describe("MovieController.getMovies", () => {
   it("should have a fetchMovies function", () => {
     expect(typeof MovieController.fetchMovies).toBe("function");
+  })
+
+  it("should call ServiceMovie.findAll()", () => {
+    MovieController.fetchMovies(req, res);
+    expect(MovieService.findAll).toHaveBeenCalled()
   })
 })
