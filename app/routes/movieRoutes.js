96a44267e48561b5841203a797bdff8b786f4c31
Carlos Marinho <carlos.marinho@mytime.com>
2022-11-12 00:05:38 -0300

    removed new way of import cause need to find fix to use this import with jest, and I was losing much time

--- app/routes/movieRoutes.js
+++ app/routes/movieRoutes.js
@@ -5,3 +5,5 @@ const router = express.Router();
 
 router.get("/", fetchMovies);
 router.post("/", createMovie);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client'
+const express = require('express');
+// import { PrismaClient } from '@prisma/client'
 
 const app = express();
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
