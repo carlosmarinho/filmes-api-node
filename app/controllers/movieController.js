08b57ebec31000088014b53f5025f0115eaea97f
Carlos Marinho <carlos.marinho@mytime.com>
2022-11-12 01:39:06 -0300

    Begin the load films from external api

--- app/controllers/movieController.js
+++ app/controllers/movieController.js
@@ -1,5 +1,14 @@
 const MovieService = require('../services/movieService')
 
+exports.loadFilms = async (req, res, next) => {
+  try {
+    await MovieService.loadFilms();
+  }
+  catch (err) {
+    next(err)
+  }
+}
+
 exports.createMovie = async (req, res, next) => {
   try {
     const movie = await MovieService.createMovie(req.body);
@@ -11,11 +20,18 @@ exports.createMovie = async (req, res, next) => {
 }
 
 exports.fetchMovies = async (req, res, next) => {
+
+  console.log("req: ", req.params)
+  const all = req.query.all || false;
+  const limit = parseInt(req.query.limit) || 1;
+  const offset = parseInt(req.query.offset) || 10
+
   try {
-    const movies = await MovieService.findAll();
+    const movies = await MovieService.findAll({ all, limit, offset });
     res.status(200).json(movies);
   }
   catch (err) {
     next(err)
   }
 }
+
--- app/services/movieService.js
+++ app/services/movieService.js
@@ -1,18 +1,37 @@
 const Prisma = require('@prisma/client');
+const axios = require('axios');
 const prisma = new Prisma.PrismaClient();
+
+exports.loadFilms = async () => {
+  try {
+    const movies = await axios.get('https://ghibliapi.herokuapp.com/films');
+
+  }
+  catch (err) {
+    console.log("err: ");
+  }
+}
 
 exports.createMovie = async (movie) => {
 
   console.log('\n\n***\n Movie: ', movie, '\n***\n');
 
-  await prisma.movie.create({
+  return await prisma.movie.create({
     data: {
       ...movie,
     }
   })
 }
 
-exports.findAll = async () => {
-  const movies = await prisma.movie.findMany();
+exports.findAll = async (params) => {
+  let movies;
+  console.log('\n\n***\n perpage: ', params.offset, params.limit, '\n***\n');
+  if (params.all)
+    movies = await prisma.movie.findMany();
+  else
+    movies = await prisma.movie.findMany({
+      skip: params.offset,
+      take: params.limit
+    });
   return movies;
 } 
